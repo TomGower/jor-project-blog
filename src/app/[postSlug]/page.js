345f49061c9e0e898a691266cc00770c44f8a04e
TomGower <tom.gower@gmail.com>
2023-08-23 17:44:07 -0500

    Exercise 4 completed?

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -6,6 +6,7 @@ import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { BLOG_TITLE } from '@/constants';
+import CodeSnippet from '@/components/CodeSnippet/CodeSnippet';
 
 export async function generateMetadata({ params }) {
   const post = await loadBlogPost(params.postSlug);
@@ -27,7 +28,7 @@ async function BlogPost({ params }) {
         publishedOn={new Date(post.frontmatter.publishedOn)}
       />
       <div className={styles.page}>
-        <MDXRemote source={post.content} />
+        <MDXRemote source={post.content} components={{ pre: CodeSnippet }} />
       </div>
     </article>
   );
